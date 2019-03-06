ffc8ae1cb14426bc594a66a0ed39ebc6044b24df
Mr.huang <2316694914@qq.com>
2019-03-06 18:01:03 +0800

    分辨环境不同域名

--- src/store.config.js
+++ src/store.config.js
@@ -3,9 +3,12 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
 Vue.use(Vuex);
+
+const host = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://adm.hqboke.cn';
+console.log(host)
 function fetchItem(id){
     //该函数是运行在node环境 所以需要加上域名
-    return axios.post('http://localhost:8080/cms/i1/user_info');
+    return axios.post(host+'/cms/i1/user_info');
 }
 export function createStore() {
     return new Vuex.Store({
